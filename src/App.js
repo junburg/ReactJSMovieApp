639dd43a95076ceadc243fd9b4d4a14e29cdd746
Junburg <ahn428@gmail.com>
2018-04-15 18:09:04 +0900

    React JS Movie app done
    Good Job!

--- src/App.js
+++ src/App.js
@@ -1,58 +1,53 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Movie from "./Movie";
 
 class App extends Component {
+  // Render: componentWillMount() -> render() -> componentDidMount()
+  // Update componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate()
+
   state = {};
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        movies: [
-          {
-            title: "Matrix",
-            poster:
-              "https://ia.media-imdb.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg"
-          },
-          {
-            title: "Full Metal Jacket",
-            poster:
-              "https://ia.media-imdb.com/images/M/MV5BNzc2ZThkOGItZGY5YS00MDYwLTkyOTAtNDRmZWIwMGRhYTc0L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg"
-          },
-          {
-            title: "Oldboy",
-            poster:
-              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRv3xvaIKiAnbSPiHLVwNn5TcbnoSWTY_YAYfUxIwjtCKTx-V3v"
-          },
-          {
-            title: "Starwars",
-            poster:
-              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSRtiF2CJ9M8gdeN1_EuaULo-0sKYvD7DzUwPeQojexfBN39uTMg"
-          },
-          {
-            title: "Trainspotting",
-            poster:
-              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFbzezTo6JCGcFCm0ms6HXr8ZzLllfrOUpnS2pebLwpCKUSY7Y"
-          }
-        ]
-      });
-    }, 1000);
+    this._getMovies();
   }
 
   _renderMovies = () => {
-    {
-      const movies = this.state.movies.map((movie, index) => {
-        return <Movie title={movie.title} poster={movie.poster} key={index} />;
-      });
-      return movies;
-    }
+    const movies = this.state.movies.map(movie => {
+      return (
+        <Movie
+          title={movie.title_english}
+          poster={movie.large_cover_image}
+          key={movie.id}
+          genres={movie.genres}
+          synopsis={movie.synopsis}
+        />
+      );
+    });
+    return movies;
+  };
+
+  _getMovies = async () => {
+    const movies = await this._callApi();
+    this.setState({
+      movies
+    });
+  };
+
+  _callApi = () => {
+    return fetch(
+      "https://yts.am/api/v2/list_movies.json?sort_by=download_count"
+    )
+      .then(potato => potato.json())
+      .then(json => json.data.movies)
+      .catch(err => console.log(err));
   };
 
   render() {
+    const { movies } = this.state;
     return (
-      <div className="App">
-        {this.state.movies ? this._renderMovies() : "Loading"}
+      <div className={movies ? "App" : "App--loading"}>
+        {movies ? this._renderMovies() : "Loading"}
       </div>
     );
   }
